e08b5808b929c093286a27e216aaed325235798f
Andrea Strnadlova <an.strnadlova@gmail.com>
2022-04-10 12:24:40 +0200

    completed task

--- main.js
+++ main.js
@@ -9,3 +9,20 @@ která vypíše kalorickou hodnotu potraviny.
 BONUS: počítej a zobrazuj celkový počet sněděných kalorií.
 Tj. při klikání na obrázky se postupně sčítají kalorie.
 */
+
+const ovoce = document.querySelectorAll('.obrazek-ovoce');
+let celkemKalorii = 0;
+
+ovoce.forEach((jednoOvoce) => {
+    jednoOvoce.addEventListener('click', zobrazKalorie);
+})
+
+function zobrazKalorie(event) {
+    let jednoOvoce = event.target;
+    document.getElementById('odpoved').innerHTML ="Snědla jsi " + jednoOvoce.dataset.kalorie + "kcal/100g.";
+    celkemKalorii = celkemKalorii + parseInt(jednoOvoce.dataset.kalorie);
+    document.getElementById('vysledek').innerHTML ="Tohle je snědeno ( celkem " + celkemKalorii + "kalorii.)";
+    let snedeneOvoce = document.getElementById('snedeno');
+    snedeneOvoce.appendChild(jednoOvoce);
+    jednoOvoce.removeEventListener('click',zobrazKalorie);
+}
